97581aba4d3f9da6f320b147ef40ab6b9b052a00
SURFTOP\Jan <jan@obermeier.ch>
2018-09-12 18:58:02 +0200

    basic functinos added and commented
    yeet

--- sjfworkshop.c
+++ sjfworkshop.c
@@ -5,6 +5,8 @@ PROCESS(main_proc, "MAIN PROCESS");
 AUTOSTART_PROCESSES(&main_proc);
 /*---------------------------------------------------------------------------*/
 
+// * MAIN FUNCTION
+// *******************************************
 
 PROCESS_THREAD(main_proc, ev, data)
 {
@@ -21,6 +23,99 @@ PROCESS_THREAD(main_proc, ev, data)
 
         }
     }
-
     PROCESS_END();
+}
+
+
+static void establishConn(uint8_t destID) {
+
+}
+
+static void sendMsg(uint8_t destID, struct msg m) {
+
+}
+
+
+// * broadcast
+// ***********************************************
+
+// called when data is received
+static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t * from) {
+
+}
+
+
+// * ping
+// ***********************************************
+
+// returns ping-struct with values
+static struct ping createPing(uint8_t destID) {
+
+}
+
+// takes raw broadcast input, interprets as ping and ingores/registers/forwards
+static void processPing(char* data) {
+
+}
+
+// returns whether given ping has already been received
+static bool isDuplicate(struct ping p) {
+
+}
+
+// process ping for forwarding and sends it
+static void pingOut(struct ping p) {
+
+}
+
+
+// * revPing
+// ***********************************************
+
+// returns revPing-struct with values
+static struct revPing createRevPing(struct ping p) {
+
+}
+
+// takes raw broadcast input, interprets as revPing and registers conn/forwards
+static void processRevPing(char* data) {
+
+}
+
+// process revPing for forwarding and sends it
+static void revPingOut(struct revPing rp) {
+
+}
+
+
+// * connection
+// ***********************************************
+
+// registers connection with revPing given
+static void regConn(struct revPing rp) {
+
+}
+
+// returns whether connection to destination has been establisched
+static bool connEstablished(uint8_t destID) {
+
+}
+
+
+// * message
+// ***********************************************
+
+// returns message-struct with values given
+static struct msg createMsg(uint8_t destID, char* text) {
+
+}
+
+// takes raw broadcast input and interprets it as message
+static void processMsg(char* data) {
+
+}
+
+// sends message
+static void msgOut(struct msg m) {
+
 }
--- sjfworkshop.h
+++ sjfworkshop.h
@@ -9,35 +9,67 @@
 #include <stdbool.h>
 
 static const struct msg {
-
+    uint8_t type;
+    uint8_t srcID;
+    uint8_t destID;
+    uint8_t prevNodeID;
+    char msg[50];
 } ;
 
 static const struct ping {
-
+    uint8_t type;
+    uint8_t srcID;
+    uint8_t destID;
+    uint8_t prevNodeID;
+    uint8_t cost;
 } ;
 
 static const struct revPing {
-
-} ;
-
-static const struct nodeTrace {
-
+    uint8_t type;
+    uint8_t srcID;
+    uint8_t destID;
+    uint8_t nextNodeID;
 } ;
 
 static const struct connection {
-
+    uint8_t srcID;
+    uint8_t destID;
+    uint8_t prevNodeID;
+    uint8_t nextNodeID;
 } ;
 
+// * global variables
+// ***********************************************
 static struct broadcast_conn broadcast;
-static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t * from);
 static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
+static struct ping pingList[10];
+static struct connection connList[5];
+
+// * functions
+// ***********************************************
+
+static void establishConn(uint8_t destID);
+static void sendMsg(uint8_t destID, struct msg m);
+
+// broadcast
+static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t * from);
+
+// ping
 static struct ping createPing(uint8_t destID);
-static struct ping createMsg(uint8_t destID);
-static void pingIn(struct ping p);
-static void pingOut(struct ping p);
-static void revPingIn(struct revPing rP);
-static void revPingOut(struct revPing rP);
+static void processPing(char* data);
 static bool isDuplicate(struct ping p);
-static void msgIn(struct msg m);
+static void pingOut(struct ping p);
+
+// revPing
+static struct revPing createRevPing(struct ping p);
+static void processRevPing(char* data);
+static void revPingOut(struct revPing rp);
+
+// connection
+static void regConn(struct revPing rp);
+static bool connEstablished(uint8_t destID);
+
+// message
+static struct msg createMsg(uint8_t destID, char* text);
+static void processMsg(char* data);
 static void msgOut(struct msg m);
-static void regConn(struct revPing rP);
