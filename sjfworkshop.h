c9c689381b42aebeb41ec6a9c95c18cdaf191495
Yayasmile <yayasmile1275@gmail.com>
2018-09-12 17:15:15 +0200

    Initital Commit

--- sjfworkshop.h
+++ sjfworkshop.h
@@ -6,6 +6,7 @@
 #include "dev/leds.h"
 #include "dev/serial-line.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 static const struct msg {
 
@@ -15,15 +16,28 @@ static const struct ping {
 
 } ;
 
+static const struct revPing {
+
+} ;
+
+static const struct nodeTrace {
+
+} ;
+
 static const struct connection {
 
 } ;
 
 static struct broadcast_conn broadcast;
 static void broadcast_recv(struct broadcast_conn *c, const linkaddr_t * from);
-
-static void sendPing(struct ping p) {
-
-}
-
 static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
+static struct ping createPing(uint8_t destID);
+static struct ping createMsg(uint8_t destID);
+static void pingIn(struct ping p);
+static void pingOut(struct ping p);
+static void revPingIn(struct revPing rP);
+static void revPingOut(struct revPing rP);
+static bool isDuplicate(struct ping p);
+static void msgIn(struct msg m);
+static void msgOut(struct msg m);
+static void regConn(struct revPing rP);
